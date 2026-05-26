22f3e8cc2a696031528fbb6c080527cdc38a22d1
omega-fallon <grbacs1970@gmail.com>
2026-05-25 22:36:47 -0400

    Correction.

--- src/gameState.ts
+++ src/gameState.ts
@@ -202,8 +202,10 @@ export function replayPath(
         state,
       )}\n`,
     );
-    if (isGoal(state, target, requireFinalJump)) console.log("Goal reached!");
-    console.log(path);
+    if (isGoal(state, target, requireFinalJump)) {  
+      console.log("Goal reached!");
+      console.log(path);
+    }
   }
 }
 
